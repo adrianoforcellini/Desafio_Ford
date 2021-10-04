acb1ca0a69e4ffcf96721fb9e5c942ec09c7ff7b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-04 10:54:40 -0300

    Primeiro passo -> encontrar a regiao

--- desafio.js
+++ desafio.js
@@ -1,25 +1,75 @@
-
 //Capitais = [“Porto Velho”,”Manaus”,”Rio Branco”,”Campo Grande”,”Macapá”,”Brasília”,”Boa Vista”,”Cuiabá”,”Palmas”,”São Paulo”,”Teresina”,”Rio de Janeiro”,”Belém”,”Goiânia”,”Salvador”,”Florianópolis”,”São Luís”,”Maceió”,”Porto Alegre”,”Curitiba”,”Belo Horizonte”,”Fortaleza”,”Recife”,”João Pessoa”,”Aracaju”,”Natal “,”Vitória”]
 
-
-// N 
+// N
 // [Manaus, Belém, Rio Branco, Porto Velho, Boa Vista, Macapá, Palmas]
 
 //NE:
-//[São Luís, Teresina, Fortaleza, Natal, Jão Pessoa, Recife, Maceió, Aracaju.]
+// [São Luís, Teresina, Fortaleza, Natal, Jão Pessoa, Recife, Maceió, Aracaju.]
 
-// CO: 
+// CO:
 // [Cuiabá, Campo Grande, Goiânia, Brasília.]
 
 // SE:
 // [São Paulo, Belo Horizonte, Rio de Janeiro, Espírito Santo	]
 
-// Fronteiras: 
+// S:
+// [Curitiba, Porto Alegre, Florianópolis ]
+
+// Fronteiras:
 // N -> NE, CO
 // NE -> SE, CO, N
 
-// CO -> N, NO, SE, S -> Nosso coringa. se não houver rota direta, serão duas. 
+// CO -> N, NO, SE, S -> Nosso coringa. se não houver rota direta, serão duas.
 // Uma que vai pro Coentro Oeste, e outra pro destino final.
 
 // SE -> CO, S, NE
 // S -> SE, CO
+
+const findRegion = (capital) => {
+  const N = [
+    "Manaus",
+    "Belém",
+    "Rio Branco",
+    "Porto Velho",
+    "Boa Vista",
+    "Macapá",
+    "Palmas",
+  ];
+  const NE = [
+    "São Luís",
+    "Teresina",
+    "Fortaleza",
+    "Natal",
+    "João Pessoa",
+    "Recife",
+    "Maceió",
+    "Aracaju",
+  ];
+  const CO = ["Cuiabá", "Campo Grande", "Goiânia", "Brasília"];
+  const SE = [
+    "São Paulo",
+    "Belo Horizonte",
+    "Rio de Janeiro",
+    "Espírito Santo",
+  ];
+  const S = ["Curitiba", "Porto Alegre", "Florianópolis "];
+
+  if (N.includes(capital)) {
+    return "N";
+  }
+  if (NE.includes(capital)) {
+    return "NE";
+  }
+  if (CO.includes(capital)) {
+    return "CO";
+  }
+  if (SE.includes(capital)) {
+    return "SE";
+  }
+  if (SE.includes(capital)) {
+    return "SE";
+  }
+  if (S.includes(capital)) {
+    return "S";
+  }
+};
